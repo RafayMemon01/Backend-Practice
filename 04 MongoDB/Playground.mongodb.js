8c2bdebc29dc6548e6f698951d845045ac3ede08
RafayMemon01 <rafayrashid200611@gmail.com>
2024-09-04 12:14:01 +0500

    mongoDB practice

---
+++ 04 MongoDB/Playground.mongodb.js
@@ -0,0 +1,149 @@
+use('School-Data');
+
+// Insert a few documents into the sales collection.
+db.getCollection('StudentsBio').insertMany([
+    {
+        "RoleNo": "15553",
+        "Name": "Ahmed",
+        "Class": "5th",
+        "Section": "B",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15554",
+        "Name": "Sara",
+        "Class": "5th",
+        "Section": "A",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15555",
+        "Name": "Ali",
+        "Class": "5th",
+        "Section": "C",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15556",
+        "Name": "Zainab",
+        "Class": "5th",
+        "Section": "B",
+        "Address": "Lahore"
+    },
+    {
+        "RoleNo": "15557",
+        "Name": "Bilal",
+        "Class": "5th",
+        "Section": "A",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15558",
+        "Name": "Ayesha",
+        "Class": "5th",
+        "Section": "C",
+        "Address": "Islamabad"
+    },
+    {
+        "RoleNo": "15559",
+        "Name": "Usman",
+        "Class": "5th",
+        "Section": "B",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15560",
+        "Name": "Fatima",
+        "Class": "5th",
+        "Section": "A",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15561",
+        "Name": "Hamza",
+        "Class": "5th",
+        "Section": "C",
+        "Address": "Lahore"
+    },
+    {
+        "RoleNo": "15562",
+        "Name": "Maryam",
+        "Class": "5th",
+        "Section": "B",
+        "Address": "Islamabad"
+    },
+    {
+        "RoleNo": "15563",
+        "Name": "Omar",
+        "Class": "5th",
+        "Section": "A",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15564",
+        "Name": "Anaya",
+        "Class": "5th",
+        "Section": "C",
+        "Address": "Lahore"
+    },
+    {
+        "RoleNo": "15565",
+        "Name": "Farhan",
+        "Class": "5th",
+        "Section": "B",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15566",
+        "Name": "Imran",
+        "Class": "5th",
+        "Section": "A",
+        "Address": "Islamabad"
+    },
+    {
+        "RoleNo": "15567",
+        "Name": "Khadija",
+        "Class": "5th",
+        "Section": "C",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15568",
+        "Name": "Raza",
+        "Class": "5th",
+        "Section": "B",
+        "Address": "Lahore"
+    },
+    {
+        "RoleNo": "15569",
+        "Name": "Hiba",
+        "Class": "5th",
+        "Section": "A",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15570",
+        "Name": "Daniyal",
+        "Class": "5th",
+        "Section": "C",
+        "Address": "Islamabad"
+    },
+    {
+        "RoleNo": "15571",
+        "Name": "Zara",
+        "Class": "5th",
+        "Section": "B",
+        "Address": "Karachi"
+    },
+    {
+        "RoleNo": "15572",
+        "Name": "Sami",
+        "Class": "5th",
+        "Section": "A",
+        "Address": "Lahore"
+    }
+]
+);
+
+
+console.log("Data Submitted");
